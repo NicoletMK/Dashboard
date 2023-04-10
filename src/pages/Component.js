a48497e5ceda4b3f060065bf254dcf335a276a7a
NicoletMK <nicoletianmengke@gmail.com>
2023-04-10 13:33:41 -0500

    initial commit

---
+++ src/pages/Component.js
@@ -0,0 +1,460 @@
+import styles from "./Component.module.css";
+
+const Component = () => {
+  return (
+    <div className={styles.div}>
+      <div className={styles.divParent}>
+        <div className={styles.div1}>
+          <div className={styles.xYourLead}>5x Your Lead Flow</div>
+        </div>
+        <div className={styles.div2}>
+          <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+        </div>
+        <div className={styles.loremIpsumDolor}>
+          Lorem ipsum dolor sit amet consectetur. Risus sit vel sit in quisque
+          neque. Elit amet ut pellentesque purus morbi fringilla. Urna morbi dui
+          euismod curabitur ullamcorper aenean tempus nibh.
+        </div>
+      </div>
+      <div className={styles.div3} />
+      <div className={styles.actionParent}>
+        <div className={styles.action}>
+          <b className={styles.dashboard}>Dashboard</b>
+          <img
+            className={styles.vuesaxlineargrid3Icon}
+            alt=""
+            src="/vuesaxlineargrid3.svg"
+          />
+        </div>
+        <b className={styles.realply}>Realply</b>
+        <div className={styles.action1}>
+          <b className={styles.dashboard}>Messages</b>
+          <img
+            className={styles.vuesaxlinearmessageIcon}
+            alt=""
+            src="/vuesaxlinearmessage.svg"
+          />
+        </div>
+        <div className={styles.action2}>
+          <b className={styles.calendar}>Calendar</b>
+          <img
+            className={styles.vuesaxlineargrid3Icon}
+            alt=""
+            src="/vuesaxlinearcalculator.svg"
+          />
+        </div>
+        <div className={styles.action3}>
+          <b className={styles.calendar}>Campaigns</b>
+          <img
+            className={styles.vuesaxlineargrid3Icon}
+            alt=""
+            src="/vuesaxlinearflash.svg"
+          />
+        </div>
+        <div className={styles.action4}>
+          <b className={styles.calendar}>Settings</b>
+          <img
+            className={styles.vuesaxlineargrid3Icon}
+            alt=""
+            src="/vuesaxlinearsetting3.svg"
+          />
+        </div>
+        <div className={styles.action5}>
+          <b className={styles.calendar}>Help?</b>
+          <img
+            className={styles.vuesaxlineargrid3Icon}
+            alt=""
+            src="/vuesaxlineardanger.svg"
+          />
+        </div>
+        <div className={styles.action6}>
+          <b className={styles.logOut}>Log out</b>
+          <img
+            className={styles.vuesaxlinearsendSquareIcon}
+            alt=""
+            src="/vuesaxlinearsendsquare.svg"
+          />
+        </div>
+        <img className={styles.frameChild} alt="" src="/group-185.svg" />
+        <div className={styles.frameItem} />
+        <div className={styles.frameParent}>
+          <div className={styles.groupParent}>
+            <div className={styles.yourFreeTrialIsAboutToEnWrapper}>
+              <div className={styles.yourFreeTrialContainer}>
+                <span className={styles.yourFreeTrialContainer1}>
+                  <p className={styles.yourFreeTrialIsAboutToEn}>
+                    <span>Your free trial is about to end in</span>
+                    <span className={styles.span}>{` `}</span>
+                  </p>
+                  <p className={styles.days}>10 days.</p>
+                </span>
+              </div>
+            </div>
+            <div className={styles.youWillNotContainer}>
+              <span className={styles.yourFreeTrialContainer1}>
+                <p className={styles.yourFreeTrialIsAboutToEn}>
+                  You will not be billed during your free trial.
+                </p>
+                <p className={styles.yourFreeTrialIsAboutToEn}>&nbsp;</p>
+                <p className={styles.yourFreeTrialIsAboutToEn}>
+                  To keep your projects running after the trial end, upgrade to
+                  a paid option.
+                </p>
+              </span>
+            </div>
+          </div>
+          <div className={styles.aWrapper}>
+            <div className={styles.a}>
+              <b className={styles.upgrade}>Upgrade</b>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.dashboardParent}>
+        <b className={styles.dashboard1}>Dashboard</b>
+        <div className={styles.connectionsParent}>
+          <b className={styles.connections}>Connections</b>
+          <div className={styles.groupChild} />
+        </div>
+        <div className={styles.followsParent}>
+          <b className={styles.follows}>Follows</b>
+          <div className={styles.groupItem} />
+        </div>
+        <div className={styles.connectInvitesParent}>
+          <b className={styles.connectInvites}>Connect Invites</b>
+          <div className={styles.groupInner} />
+        </div>
+      </div>
+      <div className={styles.vectorParent}>
+        <img className={styles.vectorIcon} alt="" src="/vector-1.svg" />
+        <div className={styles.groupContainer}>
+          <div className={styles.groupWrapper}>
+            <div className={styles.groupFrame}>
+              <div className={styles.groupFrame}>
+                <div className={styles.groupFrame}>
+                  <div className={styles.div4}>2632</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.followers}>Connections</div>
+          <div className={styles.parent}>
+            <b className={styles.b}>56%</b>
+            <img
+              className={styles.vuesaxlineartrendUpIcon}
+              alt=""
+              src="/vuesaxlineartrendup.svg"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.groupParent1}>
+        <div className={styles.groupWrapper1}>
+          <div className={styles.container}>
+            <div className={styles.container}>
+              <div className={styles.container}>
+                <div className={styles.div5}>286</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.invitationsSent}>Invitations sent</div>
+      </div>
+      <div className={styles.groupParent2}>
+        <div className={styles.groupWrapper4}>
+          <div className={styles.frame}>
+            <div className={styles.frame}>
+              <div className={styles.frame}>
+                <div className={styles.div6}>12</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.invitationsSent}>Pending Invitations</div>
+      </div>
+      <div className={styles.groupParent3}>
+        <div className={styles.groupWrapper7}>
+          <div className={styles.groupWrapper8}>
+            <div className={styles.groupWrapper8}>
+              <div className={styles.groupWrapper8}>
+                <div className={styles.div7}>2891</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.invitationsSent}>Profile views</div>
+      </div>
+      <div className={styles.vectorGroup}>
+        <img className={styles.vectorIcon} alt="" src="/vector-1.svg" />
+        <div className={styles.groupContainer}>
+          <div className={styles.groupWrapper10}>
+            <div className={styles.groupWrapper11}>
+              <div className={styles.groupWrapper11}>
+                <div className={styles.groupWrapper11}>
+                  <div className={styles.div8}>2667</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.followers}>Followers</div>
+          <div className={styles.parent}>
+            <b className={styles.b1}>0%</b>
+            <img
+              className={styles.vuesaxlineartrendUpIcon}
+              alt=""
+              src="/vuesaxlineartrendup1.svg"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.inner}>
+        <div className={styles.groupContainer}>
+          <div className={styles.groupWrapper13}>
+            <div className={styles.groupWrapper14}>
+              <div className={styles.groupWrapper14}>
+                <div className={styles.groupWrapper14}>
+                  <div className={styles.div9}>100</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.followers}>Connect Invites</div>
+          <div className={styles.parent}>
+            <b className={styles.b2}>60%</b>
+            <img
+              className={styles.vuesaxlineartrendUpIcon}
+              alt=""
+              src="/vuesaxlineartrendup2.svg"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.child}>
+        <div className={styles.lineParent}>
+          <div className={styles.lineDiv} />
+          <div className={styles.groupChild2} />
+          <div className={styles.groupChild3} />
+          <div className={styles.groupChild4} />
+          <div className={styles.groupChild5} />
+          <div className={styles.groupChild6} />
+          <div className={styles.groupChild7} />
+          <div className={styles.groupChild8} />
+          <div className={styles.groupChild9} />
+          <div className={styles.rectangleDiv} />
+          <div className={styles.groupChild10} />
+          <div className={styles.groupChild11} />
+          <div className={styles.div10}>0</div>
+          <div className={styles.div11}>100</div>
+          <div className={styles.div12}>200</div>
+          <div className={styles.div13}>300</div>
+          <div className={styles.div14}>400</div>
+          <div className={styles.div15}>500</div>
+          <div className={styles.div16}>600</div>
+          <div className={styles.div17}>700</div>
+          <div className={styles.div18}>800</div>
+          <div className={styles.groupChild12} />
+        </div>
+      </div>
+      <div className={styles.recentActivityParent}>
+        <b className={styles.recentActivity}>Recent Activity</b>
+        <div className={styles.alexMorganSentContainer}>
+          <b>{`Alex Morgan `}</b>
+          <span>sent you a</span>
+          <b> message.</b>
+        </div>
+        <div className={styles.mujoProsperSentContainer}>
+          <b>{`Mujo Prosper `}</b>
+          <span>sent you a</span>
+          <b> connection request.</b>
+        </div>
+        <div className={styles.upcomingTaskContainer}>
+          <span>Upcoming Task</span>
+          <b>{` - Create a campaign for designers `}</b>
+          <span>due on</span>
+          <b> 02/04/2023.</b>
+        </div>
+        <img className={styles.frameInner} alt="" src="/ellipse-5@2x.png" />
+        <img className={styles.ellipseIcon} alt="" src="/ellipse-6@2x.png" />
+        <img className={styles.frameChild1} alt="" src="/ellipse-7.svg" />
+        <img
+          className={styles.vuesaxlinear3squareIcon}
+          alt=""
+          src="/vuesaxlinear3square.svg"
+        />
+        <img className={styles.frameChild2} alt="" src="/vector-11.svg" />
+        <img className={styles.frameChild3} alt="" src="/vector-11.svg" />
+        <div className={styles.hParent}>
+          <b className={styles.h}>24h</b>
+          <div className={styles.last}>Last</div>
+          <img
+            className={styles.vuesaxlineararrowSquareDowIcon}
+            alt=""
+            src="/vuesaxlineararrowsquaredown.svg"
+          />
+        </div>
+        <div className={styles.scrollbar} />
+      </div>
+      <b className={styles.campaignAnalytics}>Campaign Analytics</b>
+      <div className={styles.groupParent6}>
+        <div className={styles.jacobFrostWrapper}>
+          <b className={styles.jacobFrost}>Jacob Frost</b>
+        </div>
+        <img className={styles.groupChild13} alt="" src="/ellipse-4@2x.png" />
+        <img
+          className={styles.vuesaxlineararrowSquareDowIcon1}
+          alt=""
+          src="/vuesaxlineararrowsquaredown1.svg"
+        />
+      </div>
+      <img className={styles.item} alt="" src="/group-34146.svg" />
+      <div className={styles.chartParent}>
+        <div className={styles.chart}>
+          <img className={styles.polygon01} alt="" src="/polygon--01.svg" />
+          <img className={styles.polygon02} alt="" src="/polygon--02.svg" />
+          <img className={styles.polygon03} alt="" src="/polygon--03.svg" />
+          <img className={styles.polygon04} alt="" src="/polygon--04.svg" />
+          <img className={styles.polygon05} alt="" src="/polygon--05.svg" />
+          <img className={styles.polygon06} alt="" src="/polygon--06.svg" />
+          <img className={styles.polygon07} alt="" src="/polygon--07.svg" />
+          <img className={styles.polygon08} alt="" src="/polygon--08.svg" />
+          <img className={styles.chartChild} alt="" src="/vector-15.svg" />
+          <div className={styles.april}>April</div>
+          <div className={styles.may}>May</div>
+          <div className={styles.june}>June</div>
+          <div className={styles.july}>July</div>
+          <div className={styles.january}>January</div>
+          <div className={styles.february}>February</div>
+          <div className={styles.march}>March</div>
+          <div className={styles.number01}>
+            <div className={styles.div19}>10</div>
+          </div>
+          <div className={styles.number02}>
+            <div className={styles.div19}>20</div>
+          </div>
+          <div className={styles.number03}>
+            <div className={styles.div19}>30</div>
+          </div>
+          <div className={styles.number04}>
+            <div className={styles.div19}>40</div>
+          </div>
+          <div className={styles.number05}>
+            <div className={styles.div19}>50</div>
+          </div>
+          <div className={styles.number06}>
+            <div className={styles.div19}>60</div>
+          </div>
+          <div className={styles.number07}>
+            <div className={styles.div19}>70</div>
+          </div>
+          <div className={styles.number08}>
+            <div className={styles.div19}>80</div>
+          </div>
+          <div className={styles.number09}>
+            <div className={styles.div19}>90</div>
+          </div>
+          <img className={styles.icon} alt="" src="/01.svg" />
+          <img className={styles.bulletIcon} alt="" src="/bullet.svg" />
+          <img className={styles.bulletIcon1} alt="" src="/bullet1.svg" />
+          <img className={styles.bulletIcon2} alt="" src="/bullet2.svg" />
+          <img className={styles.bulletIcon3} alt="" src="/bullet3.svg" />
+          <img className={styles.bulletIcon4} alt="" src="/bullet4.svg" />
+          <img className={styles.bulletIcon5} alt="" src="/bullet5.svg" />
+          <img className={styles.bulletIcon6} alt="" src="/bullet6.svg" />
+          <img className={styles.chartItem} alt="" src="/vector-16.svg" />
+          <img className={styles.bulletIcon7} alt="" src="/bullet7.svg" />
+          <img className={styles.bulletIcon8} alt="" src="/bullet8.svg" />
+          <img className={styles.bulletIcon9} alt="" src="/bullet9.svg" />
+          <img className={styles.bulletIcon10} alt="" src="/bullet10.svg" />
+          <img className={styles.bulletIcon11} alt="" src="/bullet11.svg" />
+          <img className={styles.bulletIcon12} alt="" src="/bullet12.svg" />
+          <img className={styles.bulletIcon13} alt="" src="/bullet13.svg" />
+        </div>
+        <div className={styles.info}>
+          <div className={styles.data01}>
+            <div className={styles.bullet}>
+              <div className={styles.bulletChild} />
+            </div>
+            <div className={styles.txt}>
+              <div className={styles.campaign1}>Campaign 1</div>
+              <div className={styles.data}>
+                <div className={styles.campaign1}>12,423</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.data02}>
+            <div className={styles.bullet}>
+              <div className={styles.bulletItem} />
+            </div>
+            <div className={styles.txt}>
+              <div className={styles.campaign1}>Campaign 2</div>
+              <div className={styles.data}>
+                <div className={styles.campaign1}>12,423</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.doughnutParent}>
+        <img className={styles.doughnutIcon} alt="" src="/doughnut.svg" />
+        <div className={styles.info1}>
+          <div className={styles.data011}>
+            <div className={styles.bullet}>
+              <div className={styles.bulletInner} />
+            </div>
+            <div className={styles.txt}>
+              <div className={styles.campaign1}>Invitations sent</div>
+              <div className={styles.data}>
+                <div className={styles.campaign1}>286</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.data021}>
+            <div className={styles.bullet}>
+              <div className={styles.bulletChild1} />
+            </div>
+            <div className={styles.txt}>
+              <div className={styles.campaign1}>Pending invitations</div>
+              <div className={styles.data}>
+                <div className={styles.campaign1}>12</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.data03}>
+            <div className={styles.bullet}>
+              <div className={styles.bulletChild2} />
+            </div>
+            <div className={styles.txt}>
+              <div className={styles.campaign1}>Profile views</div>
+              <div className={styles.data}>
+                <div className={styles.campaign1}>2891</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.mar10Apr10Parent}>
+        <div className={styles.mar10}>Mar 10 - Apr 10</div>
+        <img
+          className={styles.vuesaxlineararrowSquareDowIcon2}
+          alt=""
+          src="/vuesaxlineararrowsquaredown2.svg"
+        />
+        <img
+          className={styles.vuesaxlinearcalculatorIcon1}
+          alt=""
+          src="/vuesaxlinearcalculator1.svg"
+        />
+      </div>
+      <div className={styles.searchParent}>
+        <div className={styles.search}>Search</div>
+        <img
+          className={styles.vuesaxlinearsearchNormalIcon}
+          alt=""
+          src="/vuesaxlinearsearchnormal.svg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Component;
